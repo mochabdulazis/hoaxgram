3d2d406084089124a57c185cd678e0f4a12ae93d
Mochamad Abdul Azis <109265521+mochabdulazis@users.noreply.github.com>
2026-05-05 01:04:50 +0700

    hoaxgram_v1.0.0

---
+++ app.py
@@ -0,0 +1,31 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+import torch
+from transformers import AutoTokenizer, AutoModelForSequenceClassification
+
+app = FastAPI()
+
+MODEL_NAME = "Syetsuki/hoaxgram_app"
+
+# Load sekali saja
+tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+model = AutoModelForSequenceClassification.from_pretrained(MODEL_NAME)
+
+class RequestText(BaseModel):
+    text: str
+
+def predict_text(text):
+    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
+    outputs = model(**inputs)
+    probs = torch.nn.functional.softmax(outputs.logits, dim=-1)
+
+    score = torch.max(probs).item()
+    label_id = torch.argmax(probs).item()
+
+    label_map = {0: "non-hoax", 1: "hoax"}
+    return label_map[label_id], score
+
+@app.post("/predict")
+def predict(req: RequestText):
+    label, score = predict_text(req.text)
+    return {"label": label, "score": score}
